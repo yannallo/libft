e8ddf30e6a56a1641eb295d48adca043a5269e69
Yann <yallo@z2r3p8.42lyon.fr>
2022-11-14 16:49:37 +0100

    adding on github acccount

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,30 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yallo <yallo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/14 12:34:32 by yallo             #+#    #+#             */
-/*   Updated: 2022/11/14 15:03:14 by yallo            ###   ########.fr       */
+/*   Created: 2022/11/14 14:09:06 by yallo             #+#    #+#             */
+/*   Updated: 2022/11/14 16:33:11 by yallo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memcpy(void *dst, const void *src, size_t size)
+void *ft_memmove(void *dst, const void *src, size_t len)
 {
-    int i;
-    char *buf;
-    char *temp;
+	int	i;
+	char temp[len];
+	char *src2, *dst2;
 
-    i = 0;
-    buf = (char *)dst;
-    temp = (char *) src;
-    while (i < size)
-    {
-        buf[i] = temp[i];
-        i++;
-    }
-    return (dst);
+	i = 0;
+	src2 = (char *) src;
+	dst2 = (char *) dst;
+	while(i < len)
+	{
+		temp[i] = src2[i];
+		dst2[i] = temp[i];
+		printf("%d : %c %c %c\n", i, src2[i], temp[i], dst2[i]);
+		i++;
+	}
+	puts(dst2);
+	return (dst);
+}
+
+int main ()
+{
+  char src[] = "bonbon...";
+  ft_memmove (src+6, src+3, 3);
+  return 0;
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: yallo <yallo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 14:16:47 by yallo             #+#    #+#             */
-/*   Updated: 2022/11/14 13:26:03 by yallo            ###   ########.fr       */
+/*   Updated: 2022/11/14 14:10:32 by yallo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,11 @@ int				ft_isascii(int c);
 int				ft_isdigit(int c);
 int				isprint(int c);
 void			*ft_memset(void *src, int value, size_t len);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlen(const char *s);
+size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t			ft_strlen(const char *s);
 int				ft_tolower(int c);
 int				ft_toupper(int c);
+void			*ft_memcpy(void *dst, const void *src, size_t size);
+void			*ft_memmove(void *dst, const void *src, size_t len);
 
 #endif
